d5779b100831227b1a3c9d25990acf8449485821
EdwardCoventry <60180868+EdwardCoventry@users.noreply.github.com>
2023-08-22 14:56:20 +0100

    fixed mistake

--- ass2srt/ass2srt.py
+++ ass2srt/ass2srt.py
@@ -31,7 +31,7 @@ class Ass2srt:
                 line = line.lstrip("Dialogue:")
                 #  split at commas, unless in bracktes, eg  {\fad(500,500)}{\be35}樱律联萌站 bbs.ylbud.com
                 #  note that perhaps it would be better to split at commas, unless in curley brackets
-                node = utils.print_from(re.split(r'\,\s*(?![^()]*\))', line))
+                node = re.split(r'\,\s*(?![^()]*\))', line)
                 node[1] = timefmt(node[1])
                 node[2] = timefmt(node[2])
                 node[9] = re.sub(r'{.*}', "", node[9]).strip()
